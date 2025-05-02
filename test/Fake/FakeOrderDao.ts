e39b3e52cf3e558e83bd234d35dd4adaa096400d
fsouza <fsouza@NRBP-PE06TBY8>
2025-05-02 00:58:48 -0300

    Refactor Tests Place Order

---
+++ test/Fake/FakeOrderDao.ts
@@ -0,0 +1,24 @@
+import { IOrderDao } from "../../src/OrderDao";
+
+export class FakeOrderDao implements IOrderDao{
+    
+    orders: any = [];
+
+    getOrdersByAccountId(accountId: any) {
+        const order = this.orders.filter((order: any) => order.accountId === accountId);
+        return order;
+    }
+    
+    getOrderById(orderId: any) {
+        const order = this.orders.find((order: any) => order.orderId === orderId);
+        return order;
+    }
+    createOrder(order: any) {
+        const orderId = crypto.randomUUID();
+        order.orderId = orderId;        
+        order.timestamp = new Date();
+        order.status = "Created";
+        this.orders.push(order);
+        return orderId;
+    }
+}
